8201fd3dfc2dee8ec97b04d25b7d8e1b53bc2bdb
Schmied Joey <joey.schmied@he-arc.ch>
2026-03-26 13:13:47 +0100

    chore: remove code smells and add security hotspots

--- src/wallet.py
+++ src/wallet.py
@@ -13,23 +13,19 @@ class Wallet(object):
     def __init__(self, initial_amount=0):
         self.balance = initial_amount
 
-    def spend_cash(self, amount, deferred=False):
+    def spend_cash(self, amount):
         """Removes the specified amount of money from the current Wallet object"""
         if self.balance < amount:
             raise InsufficientAmount(f"Not enough available to spend {amount}")
 
         self.balance -= amount
+
+    def spend_money(self, amount):
+        self.spend_cash(amount)
 
     def add_cash(self, amount):
         """Adds the specified amount of money to the current Wallet object"""
         self.balance += amount
-
-    def spend_money(self, amount, deferred=False):
-        """Removes the specified amount of money from the current Wallet object"""
-        if self.balance < amount:
-            raise InsufficientAmount(f"Not enough available to spend {amount}")
-
-        self.balance -= amount
 
     def get_balance(self):
         """Returns the current balance of the wallet"""
